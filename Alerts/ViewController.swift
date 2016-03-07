6c5303777780bb4bcf35a795de1eb3f283760ec0
Max-IM97 <max.matuson@gmail.com>
2016-03-07 17:48:48 -0500

    first commit

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Alert(sender: UIButton) {
+        let alertController = UIAlertController(title: "Alert", message: nil, preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (UIAlertAction) in
+            //nothing
+        }))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+      
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
